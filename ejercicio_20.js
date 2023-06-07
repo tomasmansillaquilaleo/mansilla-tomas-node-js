619131747a58e74b4aecf6f59429911d4d20b4d5
tomasmansillaquilaleo <135608980+tomasmansillaquilaleo@users.noreply.github.com>
2023-06-07 09:24:34 -0300

    ejercicio_20
    ejercicio-20-node-js

---
+++ ejercicio_20.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+var num= prompt("ingrese un numero: ")
+var res,num;
+function sumattion(num,res){
+    for(let i = 1; i < num; i++) {
+        res= num + i
+        
+    }
+    return res;
+
+
+} 
+var num= prompt("ingrese un numero: ")
+
+
+
