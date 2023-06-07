3668c546a59a2415acf5fd9e7c31dae4801d0427
tomasmansillaquilaleo <135608980+tomasmansillaquilaleo@users.noreply.github.com>
2023-06-07 09:15:59 -0300

    ejercicio_14
    ejercicio-14-node-js

---
+++ ejercicio_14.js
@@ -0,0 +1,21 @@
+var incremantar=0
+divisibles=0
+
+for (var i=0 ; i<100 ; i++){
+    incrementar=incrementar+1
+    divisibles=incrementar
+    if (divisibles%2===0 && divisibles%7===0)
+        console.log("PlataForma")
+    else if (divisibles%7===0){
+        console.log("Forma")
+    }
+    else if (divisibles%2===0){
+        console.log("Plata")
+    }
+    
+    else{
+        console.log(incrementar)
+            
+        }
+    }
+            
