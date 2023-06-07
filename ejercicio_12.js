aaf14a3466ee90d10e1bb76748e6f34878388937
tomasmansillaquilaleo <135608980+tomasmansillaquilaleo@users.noreply.github.com>
2023-06-07 09:13:41 -0300

    ejercicio_12
    ejercicio-12-node-js

---
+++ ejercicio_12.js
@@ -0,0 +1,31 @@
+const prompt = require('prompt-sync')();
+
+var num1=prompt("ingrese un numero:")
+
+
+var c = 100;
+var j = 2;
+var numerosPrimos = [];
+
+for (; j < c; j++) {
+
+  if (primo(j)) {
+    numerosPrimos.push(j);
+  }
+  
+}
+
+console.log(numerosPrimos);
+
+function primo(numero) {
+
+  for (var i = 2; i < numero; i++) {
+
+    if (numero % i === 0) {
+      return false;
+    }
+
+  }
+
+  return numero !== 1;
+}
