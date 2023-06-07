4ea5a227aab8ee7454dfb1b86fc652511a4d5430
tomasmansillaquilaleo <135608980+tomasmansillaquilaleo@users.noreply.github.com>
2023-06-07 08:57:49 -0300

    ejercicio_2
    ejercicio-2-node-js

---
+++ ejercicio_2.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+var n1,n2,vsuma;
+	
+	n1 = parseInt(prompt('Ingresar numero 1: '));
+	n2 = parseInt(prompt('Ingresar numero 2: '));
+	
+	vsuma = n1 + n2;
+	console.log('La suma es '+vsuma);
+    
