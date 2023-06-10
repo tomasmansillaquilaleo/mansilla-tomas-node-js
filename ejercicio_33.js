d871ab0cf239896de465fd6a920acf4771de5b0d
tomasmansillaquilaleo <135608980+tomasmansillaquilaleo@users.noreply.github.com>
2023-06-10 16:03:25 -0300

    ejercicio_33
    ejercicio-33-node-js

---
+++ ejercicio_33.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+
+var num1=prompt("ingrese el primer numero: ");
+var num2=prompt("ingrese el segundo numero: ");
+var res= (num1+num2);
+console.log("el resultado de la suma es: ",res);
