c1f853ec062d9cebfec411b845811e0910ae7a43
tomasmansillaquilaleo <135608980+tomasmansillaquilaleo@users.noreply.github.com>
2023-06-10 16:02:17 -0300

    ejercicio_31
    ejercicio-31-node-js

---
+++ ejercicio_31.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+var num;
+
+num=parseInt("ingrese un numero por favor: ")
+function esPrimo() {
+    while (num/num && num/1) {
+        console.log("es numero ingresado es primo")
+        break 
+    }
+}
+console.log(esPrimo)
